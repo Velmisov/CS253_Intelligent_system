935565b401b4028008625b3498749750c533dadf
Vladislav Velmisov <velmisovx@gmail.com>
2018-10-23 10:05:18 +0300

    new communication with the program

--- four_in_a_row/main.cpp
+++ four_in_a_row/main.cpp
@@ -5,18 +5,37 @@ using namespace std;
 
 int main() {
 
-    auto pos = new Position("333333444444");
+    //auto pos = new Position("333333444444");
+    auto pos = new Position("");
     pos->show();
 
     auto solver = new Solver();
 
-    while (pos->get_number_of_moves() != BOARD_SIZE) {
-        int col;
-        if (pos->who_play_now() == 1) {
-            cout << "HERE" << endl;
-            auto res = solver->negamax(*pos);
-            cout << res.first + 1 << " " << res.second << endl;
+    int player;
+    do {
+        cout << "Choose number of player (1 or 2): ";
+        cin >> player;
+        if (player == 1 || player == 2)
             break;
+        cout << "Retry" << endl;
+    } while (true);
+
+    while (pos->number_of_moves != BOARD_SIZE) {
+        int col;
+        if (pos->who_play_now() != player) {
+            clock_t FOR_CLOCK = clock();
+            auto res = solver->bestMove(8, *pos, pos->who_play_now());
+
+            cout << "Time spent: " << double(clock() - FOR_CLOCK) / CLOCKS_PER_SEC << " seconds" << endl;
+            cout << res.first + 1 << " " << res.second << endl;
+            col = res.first;
+
+            if (pos->is_winning_move(col)) {
+                pos->move(col);
+                pos->show();
+                cout << "Computer won ¯\\_(ツ)_/¯" << endl;
+                break;
+            }
         }
         else {
             do {
@@ -29,12 +48,12 @@ int main() {
                     cout << "Invalid column! Try again!" << endl;
                 }
             } while (true);
-        }
-        if (pos->is_winning_move(col)) {
-            pos->move(col);
-            pos->show();
-            cout << "You're lose" << endl;
-            break;
+            if (pos->is_winning_move(col)) {
+                pos->move(col);
+                pos->show();
+                cout << "You won!" << endl;
+                break;
+            }
         }
         pos->move(col);
         pos->show();
